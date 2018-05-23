7d282a18a53dc17cd6c1d4e0f6c0b6a498332c92
rroyson <frontiersons@gmail.com>
2018-05-23 18:21:31 -0400

    adding items

--- public/script.js
+++ public/script.js
@@ -1,6 +1,12 @@
 new Vue({
   el: '#app',
   data: {
-    total: 0
+    total: 0,
+    items: [{ title: 'item 1' }, { title: 'item 2' }, { title: 'item 3' }]
+  },
+  methods: {
+    addItem: function() {
+      this.total += 9.99
+    }
   }
 })

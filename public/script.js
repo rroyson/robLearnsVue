2e506b62f0c7c02c2fe3e94a500b595221d18b2f
rroyson <frontiersons@gmail.com>
2018-05-23 18:36:26 -0400

    items to cart

--- public/script.js
+++ public/script.js
@@ -2,11 +2,13 @@ new Vue({
   el: '#app',
   data: {
     total: 0,
-    items: [{ title: 'item 1' }, { title: 'item 2' }, { title: 'item 3' }]
+    items: [{ title: 'item 1' }, { title: 'item 2' }, { title: 'item 3' }],
+    cart: []
   },
   methods: {
-    addItem: function() {
+    addItem: function(index) {
       this.total += 9.99
+      this.cart.push(this.items[index])
     }
   }
 })
